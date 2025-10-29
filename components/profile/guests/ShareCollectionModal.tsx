2f6542942795c5b6ca7e1f057019721ab276a570
jrgg9006 <jrgg90@gmail.com>
2025-10-29 11:07:55 -0500

    check

--- components/profile/guests/ShareCollectionModal.tsx
+++ components/profile/guests/ShareCollectionModal.tsx
@@ -215,6 +215,7 @@ export function ShareCollectionModal({
             <textarea
               value={editingMessage}
               onChange={(e) => setEditingMessage(e.target.value)}
+              onPointerDown={(e) => e.stopPropagation()}
               className="w-full p-3 border border-gray-300 rounded-lg resize-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               rows={3}
               maxLength={280}
