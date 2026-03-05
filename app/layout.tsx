29feba3e390f94a8ef1737bacf0c028125499495
jrgg9006 <jrgg90@gmail.com>
2026-03-05 11:10:28 -0600

    added microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,15 @@ export default function RootLayout({
             gtag('config', 'G-96BCVQ91GC');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vr3wrugj4z");
+          `}
+        </Script>
         <link rel="stylesheet" href="https://use.typekit.net/fvk8ngw.css" />
       </head>
       <body className="antialiased bg-white text-gray-900">
